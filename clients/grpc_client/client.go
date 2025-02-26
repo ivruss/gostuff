b6ce0462ccf1d7193ac1637f6f949283cd5ad858
ivruss <irusanov1204@yandex.ru>
2025-02-26 15:41:18 +0300

    GRPC client closer added

--- clients/grpc_client/client.go
+++ clients/grpc_client/client.go
@@ -2,17 +2,36 @@ package grpc_client
 
 import (
 	"fmt"
+	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
-func NewGRPCConn(endpoint string) (*grpc.ClientConn, error) {
-	conn, err := grpc.NewClient(
+type GRPCConn struct {
+	conn   *grpc.ClientConn
+	logger *zap.Logger
+}
+
+func NewGRPCConn(endpoint string, logger *zap.Logger) (*GRPCConn, error) {
+	clientConn, err := grpc.NewClient(
 		endpoint,
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
 	)
+
 	if err != nil {
 		return nil, fmt.Errorf("error connecting to grpc server: %v", err)
 	}
-	return conn, nil
+
+	return &GRPCConn{
+		conn:   clientConn,
+		logger: logger,
+	}, nil
+}
+
+func (c *GRPCConn) Close(logger *zap.Logger) error {
+	c.logger.Info("closing grpc_client connection")
+	if err := c.conn.Close(); err != nil {
+		return fmt.Errorf("error closing grpc_client connection: %v", err)
+	}
+	return nil
 }
